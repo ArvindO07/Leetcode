fcdcf921b0bd1b7f7545a2fe7313dd3d7d193a11
Arvind jangir <101084834+ArvindO07@users.noreply.github.com>
2025-06-11 01:30:23 +0530

    Time: 17 ms (13.14%) | Memory: 49.1 MB (61.65%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int len = nums.length;
+        int valid = len / 3;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0)+1);
+            if (mp.get(nums[i]) > valid && !list.contains(nums[i])) {
+                list.add(nums[i]);
+            }
+        }
+        return list;
+    }
+}
