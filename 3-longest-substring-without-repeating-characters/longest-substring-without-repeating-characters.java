aee9c15b249ba62311bf1b3d9867727f6d6a5c1c
Arvind jangir <101084834+ArvindO07@users.noreply.github.com>
2025-06-28 14:08:27 +0530

    Time: 6 ms (71.69%) | Memory: 45 MB (32.16%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,43 +3,20 @@ class Solution {
         int len = s.length();
         int ans = 0;
         int countlen = 0;
-
         HashMap<Character,Integer> mp = new HashMap<>();
-
         int l = 0;
         int r = 0;
-
         while(l < len && r < len){
-            // System.out.print("s.charAt( " + r +")-> " + s.charAt(r) + " | s.charAt(" +l+") -> " + s.charAt(l) );
-            if(mp.containsKey(s.charAt(r)) && mp.get(s.charAt(r)) >= l ){
-                
+            if(mp.containsKey(s.charAt(r)) && mp.get(s.charAt(r)) >= l ){                
                 l = mp.get(s.charAt(r)) + 1;
                 mp.put(s.charAt(r), r);
             }else {
                 mp.put(s.charAt(r),r);
             }
             int temp = r-l+1;
-            // System.out.println(" | temp-> " + temp + "  | l,r-> " + l+ " , " + r + " | ans-> " + ans);
             ans = Math.max(ans,temp);
             r++;
         }
-
-
-        // for(int i = 0; i<len; i++){
-        //     HashMap<Character,Integer> mp = new HashMap<>();
-        //     for(int j = i ; j<len;j++){
-        //         mp.put(s.charAt(j), mp.getOrDefault(s.charAt(j),0) + 1);
-        //         if(mp.get(s.charAt(j)) > 1){
-        //             // System.out.println(mp.size() + " | " + " " + mp.get(s.charAt(i)) + " j-> " + j + " | " + s.charAt(j));
-        //             // ans = Math.max(mp.size(),ans);
-        //             break;
-        //         }
-        //     }
-        //     ans = Math.max(mp.size(),ans);
-        // }
-
-
-
         return ans;
 
     }
